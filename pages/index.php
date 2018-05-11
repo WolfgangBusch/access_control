392d2b416d1f3531fde57d9da6707b24451cbd9f
Wolfgang Busch <wolfgang@busch-dettum.de>
2018-05-11 18:12:18 +0000

    Create index.php

---
+++ pages/index.php
@@ -0,0 +1,3 @@
+<?php
+/** *
+ * Access Control AddOn * @author wolfgang[at]busch-dettum[dot]de Wolfgang Busch * @package redaxo5 * @version Mai 2018 */echo rex_view::title(rex_i18n::msg($this->getPackageId()));rex_be_controller::includeCurrentPageSubPath();?>
