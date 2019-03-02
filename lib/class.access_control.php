1578b6f1f84bad74f053716cca98a2bfe7051a07
Wolfgang Busch <wolfgang@busch-dettum.de>
2019-03-02 15:16:19 +0100

    Add files via upload

--- lib/class.access_control.php
+++ lib/class.access_control.php
@@ -3,7 +3,7 @@
  * Access Control AddOn
  * @author wolfgang[at]busch-dettum[dot]de Wolfgang Busch
  * @package redaxo5
- * @version Februar 2019
+ * @version März 2019
  */
 #
 class access_control {
@@ -11,19 +11,19 @@ class access_control {
 public static function member_session($func) {
    #   Returning different values depending on the value of $func
    #   $func=
-   #      "set":  authenticate (log in) the member user,
+   #      'set'   authenticate (log in) the member user,
    #              i.e. set the appropriate session variable,
-   #              return value ""
-   #      "end":  log out the member user,
+   #              return value ''
+   #      'end'   log out the member user,
    #              i.e. empty the appropriate session variable,
-   #              return value ""
-   #      "get":  return user name of the member user (= value of the
+   #              return value ''
+   #      'get'   return user name of the member user (= value of the
    #              session variable, if he is logged in)
-   #              return value "" (otherwise)
-   #      "name": return the member user's user name (as configurated)
-   #              return "", if not configured
-   #      "pwd":  return the member user's password (as configurated)
-   #              return "", if not configured
+   #              return value '' (otherwise)
+   #      'name'  return the member user's user name (as configurated)
+   #              return '', if not configured
+   #      'pwd'   return the member user's password (as configurated)
+   #              return '', if not configured
    #
    $member_login   =rex_config::get('access_control','member_login');
    $member_password=rex_config::get('access_control','member_password');
@@ -34,61 +34,55 @@ public static function member_session($func) {
    $system_id=rex_backend_login::SYSTEM_ID;
    #
    # --- set session variable
-   if($func=="set"):
+   if($func=='set'):
      $_SESSION[$instname][$system_id]['MEMBER_LOGIN']=$member_login;
      return;
      endif;
    # --- empty session variable
-   if($func=="end"):
-     $_SESSION[$instname][$system_id]['MEMBER_LOGIN']="";
+   if($func=='end'):
+     $_SESSION[$instname][$system_id]['MEMBER_LOGIN']='';
      return;
      endif;
    #
    # --- return member user's user name if he is logged in
-   if($func=="get")
-     return $_SESSION[$instname][$system_id]['MEMBER_LOGIN'];
+   if($func=='get'):
+     if(!empty($_SESSION[$instname][$system_id]['MEMBER_LOGIN'])):
+       return $_SESSION[$instname][$system_id]['MEMBER_LOGIN'];
+       else:
+       return;
+       endif;
+     endif;
    #
    # --- return member user's user name or password
-   if($func=="name") return $member_login;
-   if($func=="pwd")  return $member_password;
+   if($func=='name') return $member_login;
+   if($func=='pwd')  return $member_password;
    }
 public static function get_rex_editor() {
    #   returns die Redaxo editor's user name, if he is logged in
-   #   (or "" if not)
+   #   (or '' if not)
    #
-   $user="";
+   $user='';
    if(rex_backend_login::createUser()):
-     $user=rex::getUser()->getValue("login");
+     $user=rex::getUser()->getValue('login');
      return $user;
      endif;
    return $user;
    }
 public static function user_logged_in() {
    #   check if the visitor is logged in one of the following user:
-   #   as Redaxo editor or as member user or as YCom user,
+   #   as Redaxo editor or as member user,
    #   return an associative array containing:
-   #      $auth['redaxo']  = Redaxo editor's user name (or "")
-   #      $auth['session'] = member's user name (or "")
-   #      $auth['ycom']    = YCom user's user name (or "")
+   #      $auth['redaxo']  = Redaxo editor's user name (or '')
+   #      $auth['session'] = member's user name (or '')
    #   used functions:
    #      self::get_rex_editor()
-   #      self::member_session("get")
+   #      self::member_session('get')
    #
    # --- authenticated as Redaxo editor?
    $auth['redaxo']=self::get_rex_editor();
    #
    # --- authenticated as member user?
-   $auth['session']=self::member_session("get");
-   #
-   # --- authenticated as YCom user?
-   $auth['ycom']="";
-   if(rex_addon::get('ycom')->isAvailable()):
-     rex_ycom::addTable('rex_ycom_user');
-     rex_yform_manager_dataset::setModelClass('rex_ycom_user', rex_ycom_user::class);
-     rex_ycom_auth::login([]);
-     $us=rex_ycom_auth::getUser();
-     if($us!=NULL) $auth['ycom']=rex_ycom_user::get($us->getId())->login;
-     endif;
+   $auth['session']=self::member_session('get');
    #
    return $auth;
    }
@@ -108,9 +102,9 @@ public static function protected_or_forbidden() {
    #      $rc=access_control::protected_or_forbidden();
    #      if($rc>0):
    #        if($rc==1):
-   #          echo "... protected ...";
+   #          echo '... protected ...';
    #          else:
-   #          echo "... forbidden ...";
+   #          echo '... forbidden ...';
    #          endif;
    #        else:
    #        echo $this->getArticle(); // show the article content
@@ -151,7 +145,7 @@ public static function no_access($art,$kont) {
      # --- article in protected / forbidden area?
      $rc=2;
      if($art->getId()==$cat_id) $rc=3;
-     $arr=explode("|",$art->getValue("path"));
+     $arr=explode('|',$art->getValue('path'));
      for($i=1;$i<count($arr)-1;$i=$i+1)
         if($arr[$i]==$cat_id) $rc=3;
      #
@@ -159,7 +153,7 @@ public static function no_access($art,$kont) {
      if($rc==3):
        if($kont<=1):
          $auth=self::user_logged_in();
-         if(empty($auth['redaxo']) and empty($auth['ycom']) and empty($auth['session'])):
+         if(empty($auth['redaxo']) and empty($auth['session'])):
            $rc=0;
            endif;
          else:
@@ -190,8 +184,11 @@ public static function media2protect($file) {
    #
    # --- determine the top parent media category
    $cat=$media->getCategory();
-   $catid=$cat->getPathAsArray()[0];
-   if($catid<=0) $catid=$cat->getId();
+   if(!empty($cat->getPathAsArray()[0])):
+     $catid=$cat->getPathAsArray()[0];
+     else:
+     $catid=$cat->getId();
+     endif;
    #
    # --- top parent media category = protected media category?
    if($catid==$medcat_protected_id) return TRUE;
@@ -232,7 +229,7 @@ public static function print_file($file,$type) {
    #
    $auth=self::user_logged_in();
    if(self::media2protect($file) and
-      empty($auth['redaxo']) and empty($auth['ycom']) and empty($auth['session'])):
+      empty($auth['redaxo']) and empty($auth['session'])):
      #     error file displayed
      $errfile=rex_path::addonAssets('access_control','protected.gif');
      $managed_media=new rex_managed_media($errfile);
@@ -240,7 +237,7 @@ public static function print_file($file,$type) {
      $manager->sendMedia();
      else:
      $mtype=mime_content_type($medfile);
-     if(substr($mtype,0,5)=="image" or $mtype=="text/plain" or $mtype=="application/pdf"):
+     if(substr($mtype,0,5)=='image' or $mtype=='text/plain' or $mtype=='application/pdf'):
        #     images and pdf displayed by sendMedia()
        if(!empty($type)):
          $counter=rex_media_manager::deleteCache($file,$type);
@@ -259,84 +256,108 @@ public static function print_file($file,$type) {
 public static function login_page() {
    #   Displaying a login page for a visitor to get authenticated
    #   used functions:
-   #      self::member_session("name");
-   #      self::member_session("pwd");
+   #      self::member_session('name');
+   #      self::member_session('pwd');
    #      self::user_logged_in();
-   #      self::member_session("set");
+   #      self::member_session('set');
    #
+   # --- 1 page source for 2 languages
+   $clang_id=rex_clang::getCurrentId();
+   $clang_code=rex_clang::get($clang_id)->getCode();
+   #
+   # --- Backend
    if(rex::isBackend()):
-     echo "<p>".rex_i18n::msg("access_control_login_page_backend")."</p>\n";
+     if($clang_code=='de'):
+       echo '<p>Anzeige einer Login-Seite zur Authentifizierung als Mitglieds-Benutzer</p>';
+       else:
+       echo '<p>Displaying a login page for authentication as member user</p>';
+       endif;
+     return;
+     endif;
+   #
+   # --- Frontend
+   $member=self::member_session('name');
+   $mempwd=self::member_session('pwd');
+   #
+   # --- Language constants
+   if($clang_code=='de'):
+     $st_conf ='Bitte zunächst den Mitglieds-Benutzer nebst Passwort konfigurieren!';
+     $st_in_up='Bitte Benutzername und Passwort eingeben';
+     $st_wr_u ='+++ falscher Benutzername';
+     $st_in_p ='Bitte Passwort eingeben';
+     $st_wr_p ='+++ falsches Passwort';
+     $st_user ='Benutzername';
+     $st_pwd  ='Passwort';
+     $st_butt ='anmelden';
+     $st_memb ='Benutzer';
+     $st_auth ='erfolgreich eingeloggt';
+     endif;
+   if($clang_code=='en'):
+     $st_conf ='Configure the member user including password at first, please!';
+     $st_in_up='Insert user name and password, please';
+     $st_wr_u ='+++ wrong user name';
+     $st_in_p ='Insert password, please';
+     $st_wr_p ='+++ wrong password';
+     $st_user ='User name';
+     $st_pwd  ='Password';
+     $st_butt ='Sign in';
+     $st_memb ='User';
+     $st_auth ='logged in successfully';
+     endif;
+   if(empty($member) or empty($mempwd))
+     echo '<p class="access_control_error">'.$st_conf.'</p>';
+   #
+   # --- get login name and password
+   if(count($_POST)>0):
+     $login=$_POST['login'];
+     $passwd=$_POST['passwd'];
+     else:
+     $login='';
+     $passwd='';
+     endif;
+   #
+   # --- already authenticated?
+   $loggedin=FALSE;
+   $auth=self::user_logged_in();
+   $user=$auth['session'];
+   $error='';
+   if(!empty($user)):
+     $login=$user;
+     $passwd='';
+     $loggedin=TRUE;
      else:
      #
-     # --- Constants
-     $username=rex_i18n::msg("access_control_user_name");
-     $pwd     =rex_i18n::msg("access_control_password");
-     $member=self::member_session("name");
-     $mempwd=self::member_session("pwd");
-     if(empty($member) or empty($mempwd))
-       echo "<p class=\"access_control_error\">".
-          rex_i18n::msg("access_control_configure_member")."</p>\n";
-     #
-     # --- get login name and password
-     $login=$_POST["login"];
-     $passwd=$_POST["passwd"];
-     #
-     # --- already authenticated?
-     $loggedin=FALSE;
-     $auth=self::user_logged_in();
-     $user=$auth['session'];
-     if(!empty($user)):
-       $login=$user;
-       $loggedin=TRUE;
-       else:
-       #
-       # --- analysing the input values
-       $error="";
-       if($passwd!=$mempwd) $error=
-          rex_i18n::msg("access_control_wrong_password");
-       if(empty($passwd))   $error=
-          rex_i18n::msg("access_control_input_password");
-       if($login!=$member)  $error=
-          rex_i18n::msg("access_control_wrong_user_name");
-       if(empty($login))    $error=
-          rex_i18n::msg("access_control_input_user_password");
-       if(empty($error)) $loggedin=TRUE;
-       endif;
-     if($loggedin):
-       $error="<span class=\"access_control_success\">".
-          rex_i18n::msg("access_control_user")." \"".$member."\" ".
-          rex_i18n::msg("access_control_loggedin")."</span>";
+     # --- analysing the input values
+     if($passwd!=$mempwd) $error=$st_wr_p;
+     if(empty($passwd))   $error=$st_in_p;
+     if($login!=$member)  $error=$st_wr_u;
+     if(empty($login))    $error=$st_in_up;
+     if(empty($error)):
+       $error='<p class="access_control_success">'.
+          $st_memb.' \''.$member.'\' '.$st_auth.'</p>';
        # --- set login SESSION variable
-       self::member_session("set");
+       self::member_session('set');
+       else:
+       $error='<p class="access_control_error">'.$error.'</p>';
        endif;
-     #
-     # --- input form for User/password
-     $art_id=rex_article::getCurrentId();
-     $clang_id=rex_clang::getCurrentId();
-     $self=rex_getUrl($art_id,$clang_id);
-     echo "<div class=\"access_control_indent\">\n".
-        "<div class=\"access_control_frame\">\n".
-        "<p class=\"access_control_error\">".$error."</p>\n".
-        "<form action=\"".$self."\" method=\"post\">\n".
-        "<table>\n".
-        "    <tr><td>".$username.": &nbsp;</td>\n".
-        "        <td><input type=\"text\" name=\"login\" ".
-        "value=\"".$login."\" ".
-        "class=\"form-control access_control_width\" /></td>".
-        "</tr>\n".
-        "    <tr><td>".$pwd.":</td>\n".
-        "        <td><input type=\"password\" name=\"passwd\" ".
-        "value=\"".$passwd."\" ".
-        "class=\"form-control access_control_width\" /></td>".
-        "</tr>\n".
-        "    <tr><td>&nbsp;</td>\n".
-        "        <td><input class=\"form-control\" ".
-        "type=\"submit\" value=\"anmelden\" /></td></tr>\n".
-        "</table>\n".
-        "</form>\n".
-        "</div>\n".
-        "</div>\n";
      endif;
+   #
+   # --- input form for user/password
+   echo '
+<div class="access_control_indent"><div class="access_control_frame">
+'.$error.'
+<form method="post">
+<table>
+    <tr><td>'.$st_user.': &nbsp;</td>
+        <td><input type="text" name="login" value="'.$login.'" class="form-control access_control_width" /></td></tr>
+    <tr><td>'.$st_pwd.':</td>
+        <td><input type="password" name="passwd" value="'.$passwd.'" class="form-control access_control_width" /></td></tr>
+    <tr><td>&nbsp;</td>
+        <td><input class="form-control" type="submit" value="'.$st_butt.'" /></td></tr>
+</table>
+</form>
+</div></div>
+';
    }
 }
 ?>
--- lib/class.access_control_install.php
+++ lib/class.access_control_install.php
@@ -3,7 +3,7 @@
  * Access Control AddOn
  * @author wolfgang[at]busch-dettum[dot]de Wolfgang Busch
  * @package redaxo5
- * @version Februar 2019
+ * @version März 2019
  */
 #
 class access_control_install {
@@ -16,43 +16,60 @@ public static function sql_action($sql,$query) {
    #
    try {
         $sql->setQuery($query);
-        $error="";
+        $error='';
          } catch(rex_sql_exception $e) {
         $error=$e->getMessage();
         }
    if(!empty($error)) echo rex_view::error($error);
    }
-public static function define_modul_out() {
-   #   returning the module source code (output part)
+public static function define_module($mypackage) {
+   #   defining the module source and returning it as array:
+   #      $mod['name']     the module's name
+   #      $mod['input']    source of the module's input part
+   #      $mod['output']   source of the module's output part
+   #   $mypackage          package name
    #
-   return
-'<?php
+   $name='Member Login Page ('.$mypackage.')';
+   $in='<?php
+$clang_id=rex_clang::getCurrentId();
+$clang_code=rex_clang::get($clang_id)->getCode();
+if($clang_code=="de")
+  echo "<p>Anzeige einer Login-Seite zur Authentifizierung als Mitglieds-Benutzer</p>\n";
+if($clang_code=="en")
+  echo "<p>Displaying a login page for authentication as member user</p>\n";
+?>';
+   $out='<?php
 access_control::login_page();
 ?>';
+   return array('name'=>$name, 'input'=>str_replace('\\','\\\\',$in), 'output'=>str_replace('\\','\\\\',$out));
    }
-public static function insert_module($mypackage) {
-   #   creating the module (output part)
+public static function build_module($mypackage) {
+   #   creating / updating a module in table rex_module
+   #   $mypackage          package name
    #   functions used:
-   #      self::define_modul_out()
-   #      self::sql_action($sql,$query)
+   #      self::define_module($mypackage)
    #
-   # --- module content (output)
-   $out=self::define_modul_out();
-   $sql=rex_sql::factory();
-   $table="rex_module";
-   $modname="Member Login Page (".$mypackage.")";
+   $table='rex_module';
+   #
+   # --- module source: name input, output
+   $modul=self::define_module($mypackage);
+   $name  =$modul['name'];
+   $input =$modul['input'];
+   $output=$modul['output'];
    #
    # --- module exists already?
-   $query="SELECT * FROM ".$table." WHERE name LIKE '%".$mypackage."%'";
+   $sql=rex_sql::factory();
+   $query='SELECT * FROM '.$table.' WHERE name LIKE \'%'.$mypackage.'%\'';
    $mod=$sql->getArray($query);
-   if(count($mod[0])>0):
-     #     existing:         update
-     self::sql_action($sql,"UPDATE ".$table." SET output='".$out."' ".
-        "WHERE id=".$mod[0][id]);
+   if(!empty($mod)):
+     #     existing:         update (name unchanged)
+     $id=$mod[0]['id'];
+     counter_sql_action($sql,'UPDATE '.$table.' SET  input=\''.$input.'\'  WHERE id='.$id);
+     counter_sql_action($sql,'UPDATE '.$table.' SET output=\''.$output.'\' WHERE id='.$id);
      else:
      #     not yet existing: insert
-     self::sql_action($sql,"INSERT INTO ".$table." (name,input,output) ".
-        "VALUES ('".$modname."','','".$out."')");
+     counter_sql_action($sql,'INSERT INTO '.$table.' (name,input,output) '.
+        'VALUES (\''.$name.'\',\''.$input.'\',\''.$output.'\')');
      endif;
    }
 }
