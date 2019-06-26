e5b1c8c06c59eaf87ff6ecb2bf18093e8a644e20
Wolfgang Busch <wolfgang@busch-dettum.de>
2019-06-26 16:59:25 +0200

    Add files via upload

--- pages/introduction.php
+++ pages/introduction.php
@@ -3,7 +3,7 @@
  * Access Control AddOn
  * @author wolfgang[at]busch-dettum[dot]de Wolfgang Busch
  * @package redaxo5
- * @version März 2019
+ * @version Juni 2019
  */
 require_once __DIR__.'/../help.php';
 ?>
