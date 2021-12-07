f4703b0fcfeecde0c3aa7fc07c4e9812d21dbb5d
Wolfgang Busch <wolfgang@busch-dettum.de>
2021-12-07 18:56:44 +0100

    Add files via upload

--- pages/description.php
+++ pages/description.php
@@ -3,7 +3,7 @@
  * Access Control AddOn
  * @author wolfgang[at]busch-dettum[dot]de Wolfgang Busch
  * @package redaxo5
- * @version Februar 2021
+ * @version Dezember 2021
 */
 echo '<div><b>'.rex_i18n::rawMsg('access_control_descr_head1').'</b></div>
 <div class="access_control_indent">'
